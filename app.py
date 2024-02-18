a589d573f281a0223823541e0e7df5c70b098d9e
= <lccongthanh1102@gmail.com>
2024-02-18 17:12:54 +0700

    feat: add eureka registration in app.py file and add py eureka client as a new dependencies

--- app.py
+++ app.py
@@ -1,36 +1,40 @@
 from flask import Flask, render_template, request, jsonify
 
-import os
-
-from test1 import chatbot
+from chatbot import chatbot
 from dotenv import load_dotenv
+import py_eureka_client.eureka_client as eureka_client
 
 load_dotenv()
-bot =  chatbot()
+bot = chatbot()
 
 
 app = Flask(__name__)
+app.config.from_pyfile("settings.py")
+
+eureka_client.init(
+    eureka_server=app.config["EUREKA_SERVICE"],
+    app_name=app.config["EUREKA_INSTANCE_NAME"],
+    instance_port=app.config["PORT"],
+)
+
 
 @app.route("/")
 def index():
-    return render_template('chat.html')
+    return render_template("chat.html")
 
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
-    msg = request.form["msg"]
-    input = msg
-    result = get_Chat_response(input)
-    print('input: ', input) 
-    print('result: ', result) 
-
-    return result
+    query = request.form["msg"]
+    _response = get_Chat_response(query)
+    print(f"User query: {query}")
+    print(f"Response: {_response}")
+    return _response
 
 
 def get_Chat_response(text):
     return bot.chat_public(text)
 
-   
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
